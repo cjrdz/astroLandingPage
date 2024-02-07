be2ce521fc2e6acae4dc82e90642a5b4193dd0ee
Jonathan Rdz <jonathanrdzdev@gmail.com>
2024-02-06 19:59:15 -0600

    Astro deployments settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Jrdz-sv.github.io',
+  site: 'https://jrdz-sv.github.io',
   base: '/astro-landing-practice',
 });
