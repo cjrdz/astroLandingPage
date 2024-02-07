02842b5844852578d8bc39540577359287c58a71
Jonathan Rdz <jonathanrdzdev@gmail.com>
2024-02-06 23:17:53 -0600

    Astro landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+    buildOptions: {
+    // Include all files from the src directory
+      include: ['src/**/*.astro'],
+  },
   site: 'https://cjrdz.github.io',
   base: '/astrolandingPage',
 });
